7d65e8d5afcd2152d317360f0e612da1c1007bf7
Israa Irshad <israa@qburst.com>
2023-09-29 14:14:20 +0530

    added path to view form

--- formgeeks/urls.py
+++ formgeeks/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from simpleform.views import form_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', form_view),
 ]
